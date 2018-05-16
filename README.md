deca8773dd1a558bf10b6525e3ce6f46525f709c
Hanif Fakira <fakira.work@gmail.com>
2018-05-16 16:13:22 +0100

    path variables, set port & context root via properties

--- README.md
+++ README.md
@@ -6,4 +6,10 @@ localhost:8080
 
 1. Application class with spring runner set
     - need to have a controller, as when go to uri, nothing can handle request
-2. Couple of end points  can be hit with GET request and a response is returned
+2. Couple of end points  can be hit with GET request and a response is returned in BlogController.class
+3. Use path variables in BlogController.class
+    - Set context root and port using property file
+    - http://localhost:8090/hanfak/ex/foos/1/Han
+    - http://localhost:8090/hanfak/ex/foos/1
+4. Use configuration to set port
+5. Use
--- src/main/java/com/hanfak/controllers/BlogController.java
+++ src/main/java/com/hanfak/controllers/BlogController.java
@@ -1,13 +1,15 @@
 package com.hanfak.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class BlogController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = GET)
     public String index() {
         return "You have reached the blog controller";
     }
@@ -15,5 +17,16 @@ public class BlogController {
     @GetMapping(value="/hello")
     public String helloIndex() {
         return "You have reached the blog controller, hello endpoint";
+    }
+
+    @RequestMapping(value = "/ex/foos/{id}", method = GET)
+    public String getFoosBySimplePathWithPathVariable(
+            @PathVariable("id") long id) {
+        return "Get a specific Foo with id=" + id;
+    }
+
+    @RequestMapping(value = "/ex/foos/{id}/{name}", method = GET)
+    public String getMorePathVar(@PathVariable("id") long id, @PathVariable("name") String name) {
+        return "Get a specific Foo with id=" + id + " and name: " + name;
     }
 }
